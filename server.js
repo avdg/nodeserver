8163e3f85ae8a6c1e26665004e24730f3bf52392
Anthony Van de Gejuchte <anthonyvdg@yahoo.com>
2011-09-22 19:29:28 +0200

    Be more flexible in handling routers

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var start = new Date();
 
-var fileProvider = require('./lib/fileProvider');
 var Server = require("./lib/server");
 
 var config = require('./config.js').config;
 
-var fp = new fileProvider(config.httpServe);
-
 server = new Server({
 	port: config.port,
-	routers:[fp],
+	routers: config.httpServe,
 });
 
 server.log.write('Server running (boot time: ' + (new Date() - start) + ' ms)');
