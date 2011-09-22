7d58fc90997a203dd063ee30ce83d5bb958548d7
Anthony Van de Gejuchte <anthonyvdg@yahoo.com>
2011-09-22 18:56:33 +0200

    Cleaning up some variables

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 var start = new Date();
 
 var fileProvider = require('./lib/fileProvider');
-var Log = require('./lib/log.js');
 var Server = require("./lib/server");
 
 var config = require('./config.js').config;
 
 var fp = new fileProvider(config.httpServe);
-var log = new Log();
-var routers = [fp];
 
 server = new Server({
 	port: config.port,
 	routers:[fp],
-	log: log
 });
 
-log.write('Server running (boot time: ' + (new Date() - start) + ' ms)');
+server.log.write('Server running (boot time: ' + (new Date() - start) + ' ms)');
